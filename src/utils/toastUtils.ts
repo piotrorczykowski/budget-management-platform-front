638d62d85ca506dc6a7a3dc5359ddf81e5d4be29
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-05-27 13:03:24 +0200

    Move toasts implementation to utils file

---
+++ src/utils/toastUtils.ts
@@ -0,0 +1,12 @@
+import { toast } from 'react-toastify'
+
+export const showErrorToast = (message: string) => {
+    toast.error(message, {
+        position: toast.POSITION.BOTTOM_CENTER,
+        autoClose: 500,
+        pauseOnHover: false,
+        hideProgressBar: true,
+    })
+}
+
+export const clearAllToasts = () => toast.dismiss()
