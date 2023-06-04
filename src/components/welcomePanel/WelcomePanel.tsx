15791cf5752e2f1f0d9899f10eda508f170ab00a
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-06-04 12:13:18 +0200

    Add link to app logo

--- src/components/welcomePanel/WelcomePanel.tsx
+++ src/components/welcomePanel/WelcomePanel.tsx
@@ -1,12 +1,15 @@
 import { MdOutlineToll } from 'react-icons/md'
 import styles from './WelcomePanel.module.css'
+import { Link } from 'react-router-dom'
 
 export default function WelcomePanel() {
     return (
         <div id={styles.welcomePanelWrapper}>
-            <span id={styles.appLogo}>
-                <MdOutlineToll id={styles.logo} /> BMP
-            </span>
+            <Link id={styles.homePageLink} to="/">
+                <span id={styles.appLogo}>
+                    <MdOutlineToll id={styles.logo} /> BMP
+                </span>
+            </Link>
             <p id={styles.appName}>
                 BUDGET
                 <br />
