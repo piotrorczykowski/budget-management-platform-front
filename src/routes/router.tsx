2cf9c9f2d2d08c868ac4ac44331585c7f6ab6001
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-06-17 19:50:01 +0200

    Add missing route

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -14,6 +14,7 @@ import RecordsPage from '../pages/RecordsPage'
 import BudgetsPage from '../pages/BudgetsPage'
 import AnalysisPage from '../pages/AnalysisPage'
 import SettingsPage from '../pages/SettingsPage'
+import ProfilePage from '../pages/ProfilePage'
 
 const RedirectNotAuthUser = (element: JSX.Element): JSX.Element => {
     const { accessToken } = useAuthContext()
@@ -52,6 +53,10 @@ const Router = () => {
                 <Route
                     path="/settings"
                     element={RedirectNotAuthUser(<SettingsPage />)}
+                />
+                <Route
+                    path="/profile"
+                    element={RedirectNotAuthUser(<ProfilePage />)}
                 />
             </Route>
 
