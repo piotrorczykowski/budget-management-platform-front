01c5980a0da3a89266897c16063fd4a5787e8f42
Piotr Orczykowski <orczykowski.peter@gmail.com>
2023-07-22 11:19:27 +0200

    Add customMonthAndYearPicker component

--- src/pages/AnalyticsPage.tsx
+++ src/pages/AnalyticsPage.tsx
@@ -1,12 +1,21 @@
 import { css } from '@emotion/css'
 import TopBar from '../components/TopBar'
+import CustomMontAndYearPicker from '../components/CustomMonthAndYearPicker'
+import { useState } from 'react'
 
 const styledStatisticsPageWrapper = css``
 
 export default function AnalyticsPage() {
+    const [date, setDate] = useState(new Date())
+
     return (
         <div className={styledStatisticsPageWrapper}>
-            <TopBar pageNameText={'Analytics'} />
+            <TopBar pageNameText={'Analytics'}>
+                <CustomMontAndYearPicker
+                    date={date}
+                    handleDateChange={setDate}
+                />
+            </TopBar>
         </div>
     )
 }
